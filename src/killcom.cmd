0ddcceb180e282f3b8eb2a4da3e4c77a68bd345b
Glauber Lima <glauberlima@gmail.com>
2014-08-06 09:29:44 -0300

    Reset log for MSDTC

--- src/killcom.cmd
+++ src/killcom.cmd
@@ -9,6 +9,7 @@ taskkill -im dllhost.exe -f
 taskkill -im w3wp.exe -f
 for /d %%i in ("%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\Temporary ASP.NET Files\*") do rd /s /q "%%i"
 for /d %%i in ("%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files\*") do rd /s /q "%%i"
+msdtc -resetlog
 net start msdtc
 net start comsysapp
 net start "TTI WCF Host Service"
